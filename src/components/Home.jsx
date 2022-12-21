2490458dccc02c7ad9eea01f4a7a09a655c78ab0
Laoset <xlaospt2@gmail.com>
2022-12-21 00:13:57 -0300

    comiteado2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
             <div className='flex flex-col justify-center h-full'>
                 <h2 
                 className='text-4xl sm:text-7xl font-extrabold text-white'
-                >I'm Kevin, Full Stack Developer
+                >I'm Kevinchelo, Full Stack Developer
                 </h2>
                 <p className='text-gray-500 py-4 mx-m-md'>Apasionado por el mundo IT, desde muy temprana edad conviviendo con las computadoras. Hago uso de React, Redux, Tailwind, NodeJs, Express y PostgreSQL</p>
                 <div>
