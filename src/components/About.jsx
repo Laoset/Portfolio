683a84b8a6c419992432a19c263b8b985b1dc5ae
Laoset <xlaospt2@gmail.com>
2022-12-26 03:55:44 -0300

    addAbout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,11 @@ const About = () => {
             <div className='pb-8'>
                <p className='text-4xl font-extrabold inline border-b-4 border-gray-600 dark:text-gray-900'>About</p> 
             </div>
-            <p className='text-xl text-nuevoWhite py-4 mx-m-md dark:text-black dark:font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vel explicabo provident delectus tenetur dignissimos laboriosam cumque iure fugiat quae error reprehenderit culpa numquam sapiente accusantium soluta, ullam modi. Cumque!
+            <p className='text-xl text-nuevoWhite py-4 mx-m-md dark:text-black dark:font-semibold'>Soy Alan Kevin Corman Samanamud, desde muy temprana edad fui afortunado al tener una computadora personal, la cual me pasaba horas y horas desarmando pieza por pieza y volviéndola armar, tratando de entender su funcionamiento, lo cual genero en mí un gusto por la tecnología que nunca paro de crecer. La vida me guio por una carrera profesional muy alejada al mundo IT, pero la cual me brindo muchas habilidades personales y satisfacción. Hasta que descubrí el mundo de la programación la cual me enamoro desde el primer momento que codee mi primer ' Hello World! '.
             </p>
             <br />
             <p className='text-xl text-nuevoWhite py-4 mx-m-md dark:text-black dark:font-semibold'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero earum ducimus eligendi officiis aspernatur dolores, vero perspiciatis cupiditate harum, voluptatem tenetur debitis modi explicabo quae eaque ipsum sapiente fuga?
+            Actualmente, estoy a mitad de la carrera del Bootcamp de Henry. El objetivo de este sitio web, es la de brindar la información sobre mi persona para aquel que lo necesite y quiera contactarse conmigo.
             </p>
         </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ImgPerfil from '../assets/heroImage.jpg'
+// import ImgPerfil from '../assets/heroImage.jpg'
 import { MdDoubleArrow } from 'react-icons/md';
 import { Link } from 'react-scroll';
 const Home = () => {
@@ -11,13 +11,19 @@ const Home = () => {
         <div 
         className='max-w-screen-lg mx-auto flex flex-col items-center  justify-center h-full pr-4 md:flex-row'
         >
-            <div className='flex flex-col justify-center h-full'>
+            <div className='flex flex-col justify-center  h-full'>
+                <div className='flex justify-center items-center'>
                 <h2 
                 className='text-4xl sm:text-7xl font-extrabold text-white dark:text-gray-900'
-                >I'm Kevin, Full Stack Developer
+                >Kevin, Full Stack Developer
                 </h2>
-                <p className='text-nuevoWhite py-4 mx-m-md dark:text-black dark:font-semibold'>Apasionado por el mundo IT, desde muy temprana edad conviviendo con las computadoras. Hago uso de React, Redux, Tailwind, NodeJs, Express y PostgreSQL</p>
-                <div>
+                </div>
+
+                <div className='h-[2px] bg-white mt-4 dark:bg-black'></div>
+                <div className='flex justify-center items-center'>
+                    <p className='text-nuevoWhite py-4 mx-m-md dark:text-black dark:font-semibold '>Apasionado por el Desarrollo Web</p>
+                </div>
+                <div className='flex justify-center items-center'>
                     <Link to='Proyects' smooth duration={500} className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-red-600 to-red-400 cursor-pointer'>
                         Proyectos
                         <span className='group-hover:rotate-90 duration-300 pl-1'>
@@ -25,12 +31,12 @@ const Home = () => {
                         </span>
                     </Link>
                 </div>
-            </div>
+            </div   >
 
-            <div clas>
+            {/* <div clas>
                 <img src={ImgPerfil} alt="kevin"
                     className='rounded-2xl mx-auto w-2/3 md:w-full'/>
-            </div>
+            </div> */}
         </div>
     </div>
   )
